5b1897400b6be4230e43d50be4309d35a451b2f5
Nico Lindemann <nico.lindemann@proux.net>
2019-05-17 22:03:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = (req, res) => {res.end(req.headers.host)}
+module.exports = (req, res) => {res.end(req.headers.host.replace('.abschaffen.jetzt', '').toUpperCase().replace('.', ' ') +  ' ABSCHAFFEN! JETZT!')}
