c63b7ae6d90a681a17f7a26768badb3bd848b642
Nico Lindemann <nico.lindemann@proux.net>
2023-10-14 20:07:50 +0200

    update deps, add lang attr

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  webpack(config) {
-    config.module.rules.push(
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack']
-      }
-    )
-    return config
-  }
+    reactStrictMode: true,
+    poweredByHeader: false,
+    i18n: {
+        locales: ["de"],
+        defaultLocale: "de",
+    },
+    webpack(config) {
+        config.module.rules.push(
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack']
+            }
+        )
+        return config
+    }
 }
