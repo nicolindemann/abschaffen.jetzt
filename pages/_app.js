3c760914d315c11e5780c2ef1249ece9e99b780a
Nico Lindemann <nico.lindemann@proux.net>
2024-05-25 21:02:49 +0000

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default class MyApp extends App {
   render() {
@@ -83,6 +84,7 @@ export default class MyApp extends App {
         />
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </>
     );
   }
